68172abef8c60d4402ae593e500a18010b8335ff
orevavictor <igbukuvictor@gmail.com>
2023-09-07 08:14:01 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#inclide <unistd.h>
+/**
+ * main - program printing new line to standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and tge piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
