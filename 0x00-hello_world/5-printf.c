b6b8183747bf8d305a46aba532761ed1c33676cb
orevavictor <igbukuvictor@gmail.com>
2023-09-07 06:07:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - wrote a program that prints a line uskmg printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}}
